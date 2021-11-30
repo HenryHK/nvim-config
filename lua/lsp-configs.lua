cdc9e48de125671087f713a1416b124ffaeeb870
HenryHK <henryhan.hku@gmail.com>
2021-11-30 17:04:54 +1100

    better eslint; lualine minor change to always have relative path

--- lua/lsp-configs.lua
+++ lua/lsp-configs.lua
@@ -93,10 +93,13 @@ end
 
 -- config efm
 local eslint = {
-  lintCommand = 'eslint_d -f unix --stdin --stdin-filename ${INPUT}',
+  lintCommand = 'eslint_d -f visualstudio --stdin --stdin-filename ${INPUT}',
   lintIgnoreExitCode = true,
   lintStdin = true,
-  lintFormats = {'%f:%l:%c: %m'},
+  lintFormats = {
+    "%f(%l,%c): %tarning %m",
+    "%f(%l,%c): %rror %m"
+  },
   formatCommand = 'eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}',
   formatStdin = true,
 }
--- lua/lualine-configs.lua
+++ lua/lualine-configs.lua
@@ -4,12 +4,21 @@ require('lualine').setup {
         theme = 'gruvbox',
     },
     sections = {
-    lualine_a = {
-        {
-          'filename',
-          file_status = true, -- displays file status (readonly status, modified status)
-          path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
-        }
-      }
+        lualine_a = {
+            {
+              'filename',
+              file_status = true, -- displays file status (readonly status, modified status)
+              path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+            }
+        },
+        lualine_c = {},
+    },
+    inactive_sections={
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {{'filename', file_status = true, path = 1}},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
     }
 }
